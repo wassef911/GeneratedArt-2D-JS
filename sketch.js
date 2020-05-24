4d7176081e89f98b34640171445618e8d291ca02
wassef911 <1.wassef911@gmail.com>
2020-05-24 17:54:12 +0100

    added random colors

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 const canvasSketch = require("canvas-sketch");
 const { lerp } = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
-
+const palettes = require("nice-color-palettes");
 const settings = {
   dimensions: [2048, 2048],
 };
@@ -11,11 +11,13 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
+    const palette = random.pick(palettes);
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = x / (count - 1);
         const v = y / (count - 1);
         points.push({
+          color: random.pick(palette),
           raduis: Math.abs(0.005 + random.value() * 0.01),
           position: [u, v],
         });
@@ -31,7 +33,7 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     points.forEach((circle) => {
-      const { raduis, position } = circle;
+      const { raduis, position, color } = circle;
       const [u, v] = position;
       const margin = 400;
       const x = lerp(margin, width - margin, u);
@@ -39,7 +41,7 @@ const sketch = () => {
 
       context.beginPath();
       context.arc(x, y, raduis * width, 0, Math.PI * 2);
-      context.fillStyle = "Black";
+      context.fillStyle = color;
       context.fill();
     });
   };
